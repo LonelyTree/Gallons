e1ec068266189b389ec2d47fcef1dbb891f532bb
Alex Hughes <alexhughes@AlexMBPro.local>
2019-08-21 14:42:33 -0700

    Foundation folders and assets have been  set

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Beaker from './components/Beaker'
+import "./css/app.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1 className="title">Scientific Switcheroo</h1>
+        <div className="desc"><span className="rules-heading">Name of the game:</span><span></span><p className="rules">You have two beakers that fill to 3ml and 5ml respectively. You may empty and refill each beaker as many times as you'd like. Find a way to end up with 4ml.</p></div>
+        <div className='desc--challenge'><span className="rules-challenge">Challenge:</span><span/><p className='rules'>Complete in less than 7 clicks!</p></div>
+        <div className="game">
+            <Beaker/>
+          <div className="buttons">
+            <div className='beaker1Add'></div>
+            <div className='beaker1Reduce'></div>
+            <div className='beaker2Add'></div>
+            <div className='beaker2Reduce'></div>
+            <div className=''></div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Beaker/Water/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+
+
+class Water extends Component{
+  render() {
+    return (
+              <div className='Water'>
+                {/* <h1>{Number of ml from props}</h1> */}
+              </div>
+    )
+  }
+}
+
+export default Water
