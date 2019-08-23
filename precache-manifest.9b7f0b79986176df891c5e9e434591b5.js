1cf525b70b90ad93cd5a92fad1f91c29d0d5ccbf
Alex Hughes <alexhughes@AlexMBPro.local>
2019-08-23 12:22:28 -0700

    Updates

---
+++ precache-manifest.9b7f0b79986176df891c5e9e434591b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de9e48499eacac9f7f795e2f0a99d9f2",
+    "url": "/Scientific-Switcheroo/index.html"
+  },
+  {
+    "revision": "5b707763078136db3c02",
+    "url": "/Scientific-Switcheroo/static/css/main.98825089.chunk.css"
+  },
+  {
+    "revision": "e930f6b916430f8fdcf7",
+    "url": "/Scientific-Switcheroo/static/js/2.750c5b95.chunk.js"
+  },
+  {
+    "revision": "5b707763078136db3c02",
+    "url": "/Scientific-Switcheroo/static/js/main.6edda386.chunk.js"
+  },
+  {
+    "revision": "9377c81e9dccc1262d4f",
+    "url": "/Scientific-Switcheroo/static/js/runtime~main.0fffc25f.js"
+  }
+]);
